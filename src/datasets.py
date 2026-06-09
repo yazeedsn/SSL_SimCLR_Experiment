396a9fb746aae8dd11fbe7132e4249a56f02441a
Yazeed <yazeedsn153@outlook.com>
2026-06-09 12:33:36 +0300

    fixed imports

--- src/datasets.py
+++ src/datasets.py
@@ -1,13 +1,13 @@
 from torchvision.datasets import STL10
-from config import DATASET_PATH
-import transforms
+from .config import DATASET_PATH
+from . import transforms
 
 def get_labeled_ds():
     return STL10(
         DATASET_PATH, 
         split="train", 
         download=True, 
-        transform=transforms.get_agumnetation_normalized_transform()
+        transform=transforms.get_random_augmentation_normalized_transform()
     )
 
 def get_unlabeled_ds():
